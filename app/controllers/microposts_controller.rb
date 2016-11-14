36c255019b58f034bc1938433d68598b496d49d1
lippo97 <filo_97@hotmail.it>
2016-11-14 15:48:02 +0100

    Aggiunte routes micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,13 @@
 class MicropostsController < ApplicationController
   def index
+    @microposts = Micropost.all
   end
 
   def new
+    @microposts = Micropost.new
   end
 
   def show
+    @microposts = Micropost.find(params[:id])
   end
 end
