045d5a5924db32cca90a3c0da8f22a9b95871184
a-marfoglia <a.marfoglia@hotmail.it>
2017-01-27 16:38:10 +0100

    Devise::Mailer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.mailer_sender = 'grupponet4c@gmail.com'
 
   # Configure the class responsible to send e-mails.
-   config.mailer = 'devise/mailer'
+  # config.mailer = 'MyMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
