3769d2eb9faf820177e044d1b8c8185d1ddc74a2
a-marfoglia <a.marfoglia@hotmail.it>
2017-01-27 17:30:48 +0100

    Password in chiaro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,8 +88,10 @@ Rails.application.configure do
     :port                 => 25,
     :authentication       => :plain,
     :domain               => 'live.com',
-    :user_name            => ENV["HOTMAIL_USERNAME"],
-    :password             => ENV["HOTMAIL_PASSWORD"]
+    #:user_name            => ENV["HOTMAIL_USERNAME"],
+    #:password             => ENV["HOTMAIL_PASSWORD"]
+    :user_name            => 'a.marfoglia@hotmail.it',
+    :password             => 'londra03'
   }
   
 end
