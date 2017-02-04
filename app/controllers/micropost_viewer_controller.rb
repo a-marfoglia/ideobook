ae0848df5fa84923be27f4d0adba07873811617f
a-marfoglia <a.marfoglia@hotmail.it>
2017-02-04 18:09:10 +0100

    prova

--- app/controllers/micropost_viewer_controller.rb
+++ app/controllers/micropost_viewer_controller.rb
@@ -3,6 +3,7 @@ class MicropostViewerController < ApplicationController
   
   def show
     @micropost = Micropost.find(params[:id])
-    redirect_to root_path unless @micropost.has_attachment?
+    redirect_to root_path unless @micropost && @micropost.has_attachment?
+    redirect_to @micropost.attachment.path unless @micropost.attachment.file.extension == "docx"
   end
 end
