9bdbacabc3da014966cf75a4e80e2e78639988af
a-marfoglia <a.marfoglia@hotmail.it>
2017-02-04 15:37:17 +0100

    Albi ha fatto qualcosa

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
    config.mailer_sender = 'a.marfoglia@hotmail.it'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'MyMailer'
+   config.mailer = 'MyMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
