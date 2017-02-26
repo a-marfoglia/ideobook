cac2affe15a5aa90e6e0710341416fc9d7611eba
a-marfoglia <a.marfoglia@hotmail.it>
2017-02-26 13:15:31 +0100

    deploy amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  server '52.39.215.21', user: 'deploy', roles: %w{web app db}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
