26ab006e450c1206e7b36a20732d4842ec28a83c
a-marfoglia <a.marfoglia@hotmail.it>
2017-05-22 20:25:33 +0200

    rimosso url bucket s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://elasticbeanstalk-eu-west-2-014357067258.s3-eu-west.amazonaws.com'
+  # config.action_controller.asset_host = 'http://elasticbeanstalk-eu-west-2-014357067258.s3-eu-west.amazonaws.com'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
