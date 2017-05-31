e6fbe2540eae4b88fb828608e6ae6925cd5165c5
a-marfoglia <a.marfoglia@hotmail.it>
2017-05-31 12:08:13 +0200

    modifica path avatar e tolta categoria preferita

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, presence: true, uniqueness: true, length: { maximum: 20, minimum: 6 }  
+  validates :username, presence: true, uniqueness: true, length: { maximum: 20, minimum: 6 }
   before_save { email.downcase! }
 
   def notifications_num
@@ -24,24 +24,5 @@ class User < ActiveRecord::Base
   def last_notifications(num)
     self.notifications.first(num) if num > 0
   end
-  
-  def favourite_category
-    counting = Hash.new
-    # Counts all the categories
-    self.microposts.each do |micropost|
-      counting[micropost.category_id] ||= 1
-      counting[micropost.category_id] += 1
-    end
 
-    favourite_category_key = 0
-    counting.each do |key, value|
-      highest ||= value
-      favourite_category_key =  favourite_category_key == 0 ? key : favourite_category_key
-      favourite_category_key = value > highest ? key : favourite_category_key
-    end
-    return nil if favourite_category_key == 0
-    favourite_category_key
-  end
-  
-  
 end
