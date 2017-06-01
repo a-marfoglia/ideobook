b1ac4129b6831c4d72174988e2edb995c53a7e52
a-marfoglia <a.marfoglia@hotmail.it>
2017-06-01 12:19:11 +0200

    modifiche finali

--- app/controllers/micropost_viewer_controller.rb
+++ app/controllers/micropost_viewer_controller.rb
@@ -1,6 +1,6 @@
 class MicropostViewerController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
     @micropost = Micropost.find(params[:id])
     redirect_to root_path unless @micropost && @micropost.has_attachment?
